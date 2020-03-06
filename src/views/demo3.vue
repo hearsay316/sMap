0bb2e42081ceeb00b43086d213381ec5364e8961
hearsay316 <qazwsx2228@163.com>
2020-03-06 15:51:23 +0800

    调整了小车的覆盖问题

--- src/views/demo3.vue
+++ src/views/demo3.vue
@@ -561,15 +561,27 @@ export default {
       console.log(111, value);
     },
     resourceDeploymentPanelImg3() {
+      this.Status.isMapCartPanel3 = false;
+      this.isFilterImg3 = true
+      this.Status.isMapCartPanel = false;
+      this.isFilterImg = true
       this.Status.isMapCartPanel3 = !this.Status.isMapCartPanel3;
       this.isFilterImg3 = !this.isFilterImg3;
     },
     resourceDeploymentPanelImg2() {
+      this.Status.isMapCartPanel3 = false;
+      this.isFilterImg3 = true
+      this.Status.isMapCartPanel = false;
+      this.isFilterImg = true
       this.Status.isMapCartPanel2 = !this.Status.isMapCartPanel2;
       this.isFilterImg2 = !this.isFilterImg2;
     },
     resourceDeploymentPanelImg() {
       console.log("点击添加");
+      this.Status.isMapCartPanel3 = false;
+      this.isFilterImg3 = true
+      this.Status.isMapCartPanel2 = false;
+      this.isFilterImg2 = true
       this.Status.isMapCartPanel = !this.Status.isMapCartPanel;
       this.isFilterImg = !this.isFilterImg;
     },
