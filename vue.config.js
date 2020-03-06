b1b8cae840e96a3fd63d49f56cbe853c482830c1
hearsay316 <qazwsx2228@163.com>
2020-03-06 16:01:18 +0800

    cdn  错误

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 // vue.config.js
 const UploadSshPlugin = require('./plugins/uploadSshPlugin')
 module.exports = {
-  publicPath: process.env.VUE_APP_ENV === 'production' ? 'http://cdn.j6375x.cn/cdn/webgl/' + process.env.VER : '.',
+  //publicPath: process.env.VUE_APP_ENV === 'production' ? 'http://cdn.j6375x.cn/cdn/webgl/' + process.env.VER : '.',
+  publicPath:'.',
   // 选项...
   devServer: {
     before(app) {
@@ -19,13 +20,13 @@ module.exports = {
     config.externals = {
       Cesium: "Cesium"
     };
-    if (process.env.VUE_APP_ENV === 'production') {
-      config.plugins.push(new UploadSshPlugin({
-        host: process.env.host,
-        username: process.env.name,
-        password: process.env.password,
-        port: process.env.port
-      }))
-    }
+    // if (process.env.VUE_APP_ENV === 'production') {
+    //   config.plugins.push(new UploadSshPlugin({
+    //     host: process.env.host,
+    //     username: process.env.name,
+    //     password: process.env.password,
+    //     port: process.env.port
+    //   }))
+    // }
   }
 };
