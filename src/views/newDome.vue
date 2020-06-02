9002dd8a296b3219e6eb2a930e0b672680bc128d
hearsay316 <qazwsx2228@163.com>
2020-06-02 09:57:04 +0800

    去掉了测试跳转

--- src/views/newDome.vue
+++ src/views/newDome.vue
@@ -127,10 +127,10 @@
     >
       进入场景
     </div>
-    <div class="newDome-test">
-      <router-link to="/">点击换</router-link>
-      <router-link to="/Polyline">点击换/Polyline</router-link>
-    </div>
+    <!--    <div class="newDome-test">-->
+    <!--      <router-link to="/">点击换</router-link>-->
+    <!--      <router-link to="/Polyline">点击换/Polyline</router-link>-->
+    <!--    </div>-->
   </div>
 </template>
 <!--
