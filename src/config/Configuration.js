bad848a7f21ce68c4d63ae29698ca7e64a8ae627
hearsay316 <qazwsx2228@163.com>
2020-07-20 15:09:23 +0800

    修复了函数的错误

--- src/config/Configuration.js
+++ src/config/Configuration.js
@@ -603,7 +603,7 @@ export function setView(scene, position, angle, Cesium) {
 }
 export function setPositionView(scene, position, Cesium) {
   console.log("setView 开始");
-  if (Object.keys(position).length === 3 && Object.keys(angle).length === 3) {
+  if (Object.keys(position).length === 3) {
     let { x, y, z } = position;
     scene.camera.setView({
       destination: new Cesium.Cartesian3(x, y, z)
